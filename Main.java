8945532678aed99b850c0742096f4950a7c4060f
dhakad0503 <dhakadsunil0503@gmail.com>
2024-06-09 08:04:19 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+   public static void main(String[] args) {
+    System.out.println("hello java for jnkins");
+    System.out.println(new Date());
+    System.out.println("hello guys");
+   }
+}
